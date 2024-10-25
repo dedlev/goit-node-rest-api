273bec94879e03ae530c07aa9a9ee52118df1eab
dedlev <l.popchuk@gmail.com>
2024-10-25 15:19:38 +0300

    adds saving the file to the public folder
    adds saving the file to the public folder

--- controllers/contacts-controllers.js
+++ controllers/contacts-controllers.js
@@ -1,112 +1,137 @@
-import Contact, { createContactSchema, updateContactSchema, contactStatusSchema } from "../models/Contact.js"
-import  HttpError  from "../helpers/HttpError.js";
+import fs from 'fs/promises';
+import path from 'path';
+
+import Contact, {
+  createContactSchema,
+  updateContactSchema,
+  contactStatusSchema,
+} from '../models/Contact.js';
+
+import HttpError from '../helpers/HttpError.js';
+
+const avatarPath = path.resolve('public', 'avatars');
 
 export const getAllContacts = async (req, res, next) => {
-    try {
-        const { _id: owner } = req.user;
-        const { page = 1, limit = 20, favorite, ...filterParams } = req.query;
-        const skip = (page - 1) * limit;
-        const filter = { owner };
+  try {
+    const { _id: owner } = req.user;
+    const { page = 1, limit = 20, favorite, ...filterParams } = req.query;
+    const skip = (page - 1) * limit;
+    const filter = { owner };
 
-        if(favorite !== undefined) {
-            filter.favorite = favorite === "true";
-        };
-
-        Object.assign(filter, filterParams);
-
-        const contacts = await Contact.find(filter, "-createdAt -updatedAt", { skip, limit }).populate("owner", "username email");
-        res.status(200).json(contacts);
-    } catch (error) {
-        next(error);
+    if (favorite !== undefined) {
+      filter.favorite = favorite === 'true';
     }
+
+    Object.assign(filter, filterParams);
+
+    const contacts = await Contact.find(filter, '-createdAt -updatedAt', {
+      skip,
+      limit,
+    }).populate('owner', 'username email');
+    res.status(200).json(contacts);
+  } catch (error) {
+    next(error);
+  }
 };
 
 export const getOneContact = async (req, res, next) => {
-    const { id } = req.params;
-    const { _id: owner } = req.user;
-    try {
-        const contact = await Contact.findOne({_id: id, owner});
-        if (!contact) {
-            return next(HttpError(404, `Contact with id: ${id} not found`));
-        }
-        res.status(200).json(contact);
-    } catch (error) {
-        next(error);
+  const { id } = req.params;
+  const { _id: owner } = req.user;
+  try {
+    const contact = await Contact.findOne({ _id: id, owner });
+    if (!contact) {
+      return next(HttpError(404, `Contact with id: ${id} not found`));
     }
+    res.status(200).json(contact);
+  } catch (error) {
+    next(error);
+  }
 };
 
 export const deleteContact = async (req, res, next) => {
-    const { id } = req.params;
-    const { _id: owner } = req.user;
-    try {
-        const contact = await Contact.findOneAndDelete({_id: id, owner});
-        if (!contact) {
-            return next(HttpError(404, `Contact with id: ${id} not found`));
-        }
-        res.status(200).json(contact);
-    } catch (error) {
-        next(error);
+  const { id } = req.params;
+  const { _id: owner } = req.user;
+  try {
+    const contact = await Contact.findOneAndDelete({ _id: id, owner });
+    if (!contact) {
+      return next(HttpError(404, `Contact with id: ${id} not found`));
     }
+    res.status(200).json(contact);
+  } catch (error) {
+    next(error);
+  }
 };
 
 export const createContact = async (req, res, next) => {
-    const { error } = createContactSchema.validate(req.body);
+  const { error } = createContactSchema.validate(req.body);
 
-    if (error) {
-        return next(HttpError(400, error.message));
-    }
-    
-    try {
-        const { _id: owner } = req.user;
-        // console.log(reg.file);
-        // console.log(reg.body);
-        const contact = await Contact.create({...req.body, owner});
-        res.status(201).json(contact);
-    } catch (error) {
-        next(error);
-    }
+  if (error) {
+    return next(HttpError(400, error.message));
+  }
+
+  try {
+    const { _id: owner } = req.user;
+    const { path: oldPath, filename } = req.file;
+    const newPath = path.join(avatarPath, filename);
+    await fs.rename(oldPath, newPath);
+
+    const avatar = path.join('avatars', filename);
+    const contact = await Contact.create({ ...req.body, avatar, owner });
+
+    res.status(201).json(contact);
+  } catch (error) {
+    next(error);
+  }
 };
 
 export const updateContact = async (req, res, next) => {
-    const { id } = req.params;
-    const { _id: owner } = req.user;
-    
-    if (Object.keys(req.body).length === 0) {
-        return next(HttpError(400, "Body must have at least one field"));
-    }
+  const { id } = req.params;
+  const { _id: owner } = req.user;
 
-    const { error } = updateContactSchema.validate(req.body);
-    if (error) {
-        return next(HttpError(400, error.message));
-    }
+  if (Object.keys(req.body).length === 0) {
+    return next(HttpError(400, 'Body must have at least one field'));
+  }
 
-    try {
-        const contact = await Contact.findOneAndUpdate({_id: id, owner}, req.body, { new: true });
-        if (!contact) {
-            return next(HttpError(404, `Contact with id: ${id} not found`));
-        }
-        res.status(200).json(contact);
-    } catch (error) {
-        next(error);
+  const { error } = updateContactSchema.validate(req.body);
+  if (error) {
+    return next(HttpError(400, error.message));
+  }
+
+  try {
+    const contact = await Contact.findOneAndUpdate(
+      { _id: id, owner },
+      req.body,
+      { new: true }
+    );
+    if (!contact) {
+      return next(HttpError(404, `Contact with id: ${id} not found`));
     }
+    res.status(200).json(contact);
+  } catch (error) {
+    next(error);
+  }
 };
 
 export const updateStatusContact = async (req, res, next) => {
-    const { id } = req.params;
+  const { id } = req.params;
 
-    const { error } = contactStatusSchema.validate(req.body);
-    if (error) {
-        return next(HttpError(400, error.message));
-    }
+  const { error } = contactStatusSchema.validate(req.body);
+  if (error) {
+    return next(HttpError(400, error.message));
+  }
 
-    const { favorite } = req.body;
-    try {
-        const contact = await Contact.findByIdAndUpdate(id, { favorite }, { new: true });
-        if (!contact) {
-            return next(HttpError(404, `Contact with id: ${id} not found`));
-        }
-        res.status(200).json(contact);
-    } catch (error) {
-        next(error);
+  const { favorite } = req.body;
+  try {
+    const contact = await Contact.findByIdAndUpdate(
+      id,
+      { favorite },
+      { new: true }
+    );
+    if (!contact) {
+      return next(HttpError(404, `Contact with id: ${id} not found`));
     }
+    res.status(200).json(contact);
+  } catch (error) {
+    next(error);
+  }
 };
--- routes/contacts-router.js
+++ routes/contacts-router.js
@@ -1,5 +1,5 @@
-import express from "express";
-import {isValidId, authenticate, upload } from "../middlewares/index.js"
+import express from 'express';
+import { isValidId, authenticate, upload } from '../middlewares/index.js';
 import {
   getAllContacts,
   getOneContact,
@@ -7,27 +7,25 @@ import {
   createContact,
   updateContact,
   updateStatusContact,
-} from "../controllers/contacts-controllers.js";
-// import { authenticate } from "../middlewares/index.js";
-
+} from '../controllers/contacts-controllers.js';
 
 const contactsRouter = express.Router();
 
 contactsRouter.use(authenticate);
 
-contactsRouter.get("/", getAllContacts);
+contactsRouter.get('/', getAllContacts);
 
-contactsRouter.get("/:id", isValidId, getOneContact);
+contactsRouter.get('/:id', isValidId, getOneContact);
 
-contactsRouter.delete("/:id", isValidId, deleteContact);
+contactsRouter.delete('/:id', isValidId, deleteContact);
 
 // upload.filds([{name: "avatar", maxCount: 1}]);
 // upload.array("avatar", 8)
-contactsRouter.post("/", createContact);
-// contactsRouter.post("/", upload.single("avatar"), createContact);
+// contactsRouter.post("/", createContact);
+contactsRouter.post('/', upload.single('avatar'), createContact);
 
-contactsRouter.put("/:id", isValidId, updateContact);
+contactsRouter.put('/:id', isValidId, updateContact);
 
-contactsRouter.patch("/:id/favorite", isValidId, updateStatusContact);
+contactsRouter.patch('/:id/favorite', isValidId, updateStatusContact);
 
 export default contactsRouter;
